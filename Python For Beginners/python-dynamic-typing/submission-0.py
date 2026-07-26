382915a4142236ac4ffb6316a737d84005804267
nimacortez <109187064+nimacortez@users.noreply.github.com>
2026-07-26 16:20:46 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 24.3
+print(type(variable))
+variable = False
+print(type(variable))
+variable =  "Me"
+print(type(variable))
+variable = [24, 25, 26]
+print(type(variable))

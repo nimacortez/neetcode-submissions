5addbfd7e054e63f165e0550bc7dad77c4ee68de
nimacortez <109187064+nimacortez@users.noreply.github.com>
2026-09-25 14:54:45 -0400

    Add: anagram-groups - submission-29

---
+++ Data Structures & Algorithms/anagram-groups/submission-29.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+# create map
+# iterate through each string, sort characters of string to form a key
+# append original string to the list corresponding to the key
+# return all values from hash map 
+
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
